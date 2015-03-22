ebc4a1e44c0b12aab72c1a2603d64dd99b698d1e
Devang Negandhi <devangnegandhi@gmail.com>
2015-03-22 19:41:52 -0400

    added coveralls support and set coverage thresholds to 100%

--- config/grunt/mocha_istanbul.js
+++ config/grunt/mocha_istanbul.js
@@ -4,13 +4,20 @@
  * Proprietary and confidential
  */
 module.exports = function (grunt, opts) {
+
 	return {
         unitTests: {
             src: opts.testFiles,
             options: {
 				coverageFolder: opts.coverageDir,
 				root: opts.srcDir,
-				reportFormats: ['lcovonly', 'html']
+				reportFormats: ['lcovonly', 'html'],
+				check: {
+					statements: 100,
+					branches: 100,
+					funcitons: 100,
+					lines: 100
+				}
             }
         }
 	}
