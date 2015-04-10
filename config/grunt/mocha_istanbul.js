c772e6ccc8a5171b109256c286893dc8dc4fae9b
Devang Negandhi <devangnegandhi@gmail.com>
2015-04-10 19:06:23 -0400

    add globalSuite and expect count variable in tests

--- config/grunt/mocha_istanbul.js
+++ config/grunt/mocha_istanbul.js
@@ -1,10 +1,12 @@
 module.exports = function (grunt, opts) {
 	'use strict';
+	// Add the default suite to the list of test files
+	opts.testFiles = [opts.testFiles].concat('config/mocha/defaultSuite.js');
 
 	return {
-        unitTests: {
-            src: opts.testFiles,
-            options: {
+		unitTests: {
+			src: opts.testFiles,
+			options: {
 				coverageFolder: opts.coverageDir,
 				root: opts.srcDir,
 				reportFormats: ['lcovonly', 'html'],
@@ -14,7 +16,7 @@ module.exports = function (grunt, opts) {
 					funcitons: 100,
 					lines: 100
 				}
-            }
-        }
+			}
+		}
 	};
 };
---
+++ config/mocha/defaultSuite.js
@@ -0,0 +1,38 @@
+var sinon = require('sinon');
+var chai = require('chai');
+
+chai.use(function (chaiObj) {
+	'use strict';
+	chaiObj.Assertion.addMethod('withMessage', function (msg) {
+		/* eslint-disable no-undef*/
+		_.flag(this, 'message', msg);
+		/* eslint-enable no-undef*/
+	});
+});
+
+before(function () {
+	'use strict';
+
+	chai.config.showStack = true;
+	global.expect = sinon.spy(chai, 'expect');
+});
+
+beforeEach(function () {
+	'use strict';
+
+	global.expect.reset();
+	global.expectCount = -1;
+});
+
+afterEach(function () {
+	'use strict';
+	var count = global.expect.callCount;
+
+	chai.expect(count, 'number of expect() calls failed to compare to expectCount').to.equal(global.expectCount);
+});
+
+after(function () {
+	'use strict';
+
+	global.expect.restore();
+});
