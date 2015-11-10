2bcd28aeb303f92fe3b0b5e508e7aecb7a66dc98
dnegandh <devangnegandhi@gmail.com>
2015-11-09 22:59:02 -0800

    update example to use the new globalize-express version

--- example/config.js
+++ example/config.js
@@ -9,11 +9,18 @@ var config = {
     cookieName: 'lang',
 
     // location of all the locale json files on disk
-    directory: __dirname + '/locales',
+    messages: __dirname + '/locales',
 
-    // An array of cldr data to load into globalize
-    // Checkout: https://github.com/jquery/globalize#2-cldr-content
-    localeData: [
+    
+    // We are not providing the localeData for this example here.
+    // This will lead to the default locale data from being loaded by globalize-express
+    //    
+    // But for this example, if we had to provide a custom list of locale data, it would
+    // be the one here below. The example will still run fine if the localeData property
+    // below is uncommented.
+    // 
+    // Checkout: https://github.com/jquery/globalize#2-cldr-content for info on what to use 
+/*  localeData: [
         "cldr-data/supplemental/likelySubtags",
         "cldr-data/supplemental/numberingSystems.json",
         "cldr-data/supplemental/plurals.json",
@@ -37,6 +44,7 @@ var config = {
         "cldr-data/main/de/timeZoneNames.json",
         "cldr-data/main/de/currencies.json"
     ],
+*/
 
     // Set this to true if running in development mode. This will delete cache before every access for localized string
     devMode: false
